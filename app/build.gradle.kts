e1f900cc2b545f5f092dbf7978a39f97555b0d14
MONIKAvv <monikalonewolf35@gmail.com>
2025-06-06 18:24:12 +0530

    first page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.runtime.saved.instance.state)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

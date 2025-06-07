541542d4c137373e27e1cbb3308376fb9aa57594
MONIKAvv <monikalonewolf35@gmail.com>
2025-06-07 20:24:46 +0530

    custom ui

---
+++ app/src/main/java/vv/monika/myfirstapp/LinearVerticalActivity.kt
@@ -0,0 +1,20 @@
+package vv.monika.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LinearVerticalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_linear_vertical)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
